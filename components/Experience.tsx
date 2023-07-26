6e73187d80d1c24facb93e42656e496500e0617b
Elvismutinda <elvocool@gmail.com>
2023-07-26 13:12:29 +0300

    Updates

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,40 +1,84 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import Intellisoft from "./workExperience/Intellisoft";
+import AmazonTest from "./workExperience/AmazonTest";
+import GoogleTest from "./workExperience/GoogleTest";
 import { motion } from "framer-motion";
-import ExperienceCard from "./ExperienceCard";
 
 type Props = {};
 
-function Experience({}: Props) {
-  return (
-    <motion.div
-      initial={{
-        opacity: 0,
-      }}
-      whileInView={{
-        opacity: 1,
-      }}
-      viewport={{
-        once: true,
-      }}
-      transition={{
-        duration: 1.5,
-      }}
-      className="h-screen flex flex-col relative overflow-hidden text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center"
-    >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Work Experience.
-      </h3>
+function ExperienceTest({}: Props) {
+  const [workIntellisoft, setWorkIntellisoft] = useState(true);
+  const [workAmazonTest, setWorkAmazonTest] = useState(false);
+  const [workGoogleTest, setWorkGoogleTest] = useState(false);
 
-      <div className="w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory">
-        {/* Experience Cards */}
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+  const handleIntellisoft = () => {
+    setWorkIntellisoft(true);
+    setWorkAmazonTest(false);
+    setWorkGoogleTest(false);
+  };
+
+  const handleAmazon = () => {
+    setWorkIntellisoft(false);
+    setWorkAmazonTest(true);
+    setWorkGoogleTest(false);
+  };
+
+  const handleGoogle = () => {
+    setWorkIntellisoft(false);
+    setWorkAmazonTest(false);
+    setWorkGoogleTest(true);
+  };
+
+  return (
+    <motion.div 
+    className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+    <h3 className="uppercase tracking-widest text-gray-500 text-4xl">
+    Work Experience
+  </h3>
+    <div className="h-screen items-center justify-center md:h-screen mt-10 flex flex-row overflow-hidden">
+      <div>
+        <ul className="md:w-32 flex flex-col">
+          <li
+            onClick={handleIntellisoft}
+            className={`${
+              workIntellisoft
+                ? "border-2 border-green-500 text-green-500"
+                : "border-2 hover:border-gray-700 text-gray-700"
+            } border-2 bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+          >
+            IntelliSOFT Consulting Limited
+          </li>
+          <li
+            onClick={handleAmazon}
+            className={`${
+              workAmazonTest
+                ? "border-2 border-green-500 text-green-500"
+                : "border-2 hover:border-gray-700 text-gray-700"
+            } border-2 bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+          >
+            Amazon
+          </li>
+          <li
+            onClick={handleGoogle}
+            className={`${
+              workGoogleTest
+                ? "border-2 border-green-500 text-green-500"
+                : "border-2 hover:border-gray-700 text-gray-700"
+            } border-2 bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+          >
+            Google
+          </li>
+        </ul>
       </div>
+      {workIntellisoft && <Intellisoft />}
+      {workAmazonTest && <AmazonTest />}
+      {workGoogleTest && <GoogleTest />}
+      </div>
+    </div>
     </motion.div>
   );
 }
 
-export default Experience;
+export default ExperienceTest;
---
+++ components/Test2.tsx
@@ -0,0 +1,169 @@
+import React from "react";
+
+type Props = {};
+
+function Experience2({}: Props) {
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <h3 className="uppercase tracking-widest text-gray-500 text-4xl mb-10">
+        Work Experience
+      </h3>
+      <ol className="group/list">
+        <li className="mb-12">
+          <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
+          lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
+            lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+            <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+              2023 - Present
+            </header>
+            <div className="z-10 sm:col-span-6">
+              <h3 className="font-medium leading-snug text-slate-200">
+                <div>
+                  <a
+                    href=""
+                    target="_blank"
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
+                      <span>
+                        Software Engineer ·{" "}
+                        <span className="inline-block">
+                          IntelliSOFT Consulting Limited
+                        </span>
+                      </span>
+                    </span>
+                  </a>
+                </div>
+              </h3>
+              <p className="mt-2 text-sm leading-normal">
+                Deliver high-quality, robust production code for a diverse array
+                of projects for clients including Harvard Business School,
+                Everytown for Gun Safety, Pratt Institute, Koala Health,
+                Vanderbilt University, The 19th News, and more. Provide
+                leadership within engineering department through close
+                collaboration, knowledge shares, and mentorship.
+              </p>
+              <ul className="mt-5 flex flex-wrap">
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li className="mb-12">
+          <div className="group relative grid pb-1 transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 lg:hover:!opacity-100 
+          lg:group-hover/list:opacity-50">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none 
+            lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+            <header className="z-10 mb-2 mt-1 text-xs font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2">
+              2023 - Present
+            </header>
+            <div className="z-10 sm:col-span-6">
+              <h3 className="font-medium leading-snug text-slate-200">
+                <div>
+                  <a
+                    href=""
+                    target="_blank"
+                    className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                  >
+                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block">
+                      <span>
+                        Software Engineer ·{" "}
+                        <span className="inline-block">
+                          IntelliSOFT Consulting Limited
+                        </span>
+                      </span>
+                    </span>
+                  </a>
+                </div>
+              </h3>
+              <p className="mt-2 text-sm leading-normal">
+                Deliver high-quality, robust production code for a diverse array
+                of projects for clients including Harvard Business School,
+                Everytown for Gun Safety, Pratt Institute, Koala Health,
+                Vanderbilt University, The 19th News, and more. Provide
+                leadership within engineering department through close
+                collaboration, knowledge shares, and mentorship.
+              </p>
+              <ul className="mt-5 flex flex-wrap">
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    React
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </li>
+      </ol>
+      <div className="mt-12">
+        <a
+          href=""
+          className="inline-flex items-center leading-tight font-semibold text-slate-200 group"
+        >
+          <span>
+            <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+              View Full Resume
+            </span>
+          </span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Experience2;
