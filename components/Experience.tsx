389667ab166cf8042ee37fd8b1dd608382a4eca1
Elvismutinda <elvocool@gmail.com>
2023-07-26 15:43:47 +0300

    Updated experience handler's styling

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -31,52 +31,51 @@ function ExperienceTest({}: Props) {
   };
 
   return (
-    <motion.div 
-    className="w-full md:h-screen p-2 flex items-center py-16">
+    <motion.div className="w-full md:h-screen p-2 flex items-center py-16">
       <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
-    <h3 className="uppercase tracking-widest text-gray-500 text-4xl">
-    Work Experience
-  </h3>
-    <div className="h-screen items-center justify-center md:h-screen mt-10 flex flex-row overflow-hidden">
-      <div>
-        <ul className="md:w-32 flex flex-col">
-          <li
-            onClick={handleIntellisoft}
-            className={`${
-              workIntellisoft
-                ? "border-2 border-green-500 text-green-500"
-                : "border-2 hover:border-gray-700 text-gray-700"
-            } border-2 bg-transparent hover:bg-transparent py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-          >
-            IntelliSOFT Consulting Limited
-          </li>
-          <li
-            onClick={handleAmazon}
-            className={`${
-              workAmazonTest
-                ? "border-2 border-green-500 text-green-500"
-                : "border-2 hover:border-gray-700 text-gray-700"
-            } border-2 bg-transparent hover:bg-transparent py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-          >
-            Amazon test
-          </li>
-          <li
-            onClick={handleGoogle}
-            className={`${
-              workGoogleTest
-                ? "border-2 border-green-500 text-green-500"
-                : "border-2 hover:border-gray-700 text-gray-700"
-            } border-2 bg-transparent hover:bg-[#112240] py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-          >
-            Google test
-          </li>
-        </ul>
+        <h3 className="uppercase tracking-widest text-gray-500 text-4xl">
+          Work Experience
+        </h3>
+        <div className="h-screen items-center justify-center md:h-screen mt-10 flex flex-row overflow-hidden">
+          <div>
+            <ul className="md:w-32 flex flex-col">
+              <li
+                onClick={handleIntellisoft}
+                className={`${
+                  workIntellisoft
+                    ? "border-2 border-[#64ffda] text-[#64ffda]"
+                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
+                }  border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+              >
+                IntelliSOFT Consulting Limited
+              </li>
+              <li
+                onClick={handleAmazon}
+                className={`${
+                  workAmazonTest
+                    ? "border-2 border-[#64ffda] text-[#64ffda]"
+                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
+                } border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+              >
+                Amazon test
+              </li>
+              <li
+                onClick={handleGoogle}
+                className={`${
+                  workGoogleTest
+                    ? "border-2 border-[#64ffda] text-[#64ffda]"
+                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
+                } border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
+              >
+                Google test
+              </li>
+            </ul>
+          </div>
+          {workIntellisoft && <Intellisoft />}
+          {workAmazonTest && <AmazonTest />}
+          {workGoogleTest && <GoogleTest />}
+        </div>
       </div>
-      {workIntellisoft && <Intellisoft />}
-      {workAmazonTest && <AmazonTest />}
-      {workGoogleTest && <GoogleTest />}
-      </div>
-    </div>
     </motion.div>
   );
 }
