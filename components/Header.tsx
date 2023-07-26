fc0f38a1fbf26c3c9c8f85bb3475d12f41c49dff
Elvismutinda <elvocool@gmail.com>
2023-07-26 12:51:36 +0300

    Added Header component and its content

---
+++ components/Header.tsx
@@ -0,0 +1,90 @@
+"use client";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+
+type Props = {};
+
+function Header({}: Props) {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://github.com/Elvocool"
+          fgColor="currentColor"
+          bgColor="transparent"
+          className="text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/elvocool/"
+          fgColor="currentColor"
+          bgColor="transparent"
+          className="text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+        />
+        <SocialIcon
+          url="https://twitter.com/Elvocool"
+          fgColor="currentColor"
+          bgColor="transparent"
+          className="text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/elvis-mutinda/"
+          fgColor="currentColor"
+          bgColor="transparent"
+          className="text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+        />
+        <SocialIcon
+          url="https://codepen.io/elvocool"
+          fgColor="currentColor"
+          bgColor="transparent"
+          className="text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          className="cursor-pointer text-gray-400 hover:text-[#fff] hover:scale-125 transition duration-300 ease-in-out"
+          network="email"
+          fgColor="currentColor"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
+
+export default Header;
