a10cd173e165bd024ee8d2ecfef23e38b2f11c70
Elvismutinda <elvocool@gmail.com>
2023-08-07 20:26:43 +0300

    Styling fixes

--- components/workExperience/Intellisoft.tsx
+++ components/workExperience/Intellisoft.tsx
@@ -16,25 +16,25 @@ function Intellisoft({}: Props) {
       transition={{
         delay: 0.1,
       }}
-      className="w-full"
+      className="w-full h-full -mt-10"
     >
-      <h3 className="flex gap-1 font-medium text-xl font-titleFont">
+      <h3 className="flex gap-1 font-medium text-xl ">
         Software Engineer Intern <span className="text-[#64ffda]">@</span>{" "} <span className="text-[#64ffda]">IntelliSOFT Consulting Limited</span>
       </h3>
-      <p className="text-sm mt-1 font-medium text-textDark">
+      <p className="text-sm mt-1 font-medium">
         July 2023 - Present
       </p>
       <ul className="mt-6 flex flex-col gap-3">
-        <li className="text-base flex gap-2 text-textDark">
+        <li className="text-base flex gap-2">
             <span className="text-textGreen mt-1"></span>
             Build and maintain web applications using ReactJS, NextJS, and TailwindCSS.
         </li>
-        <li className="text-base flex gap-2 text-textDark">
+        <li className="text-base flex gap-2">
             <span className="text-textGreen mt-1"></span>
             Build and maintain web applications using ReactJS, NextJS, and TailwindCSS.
             Fix bugs and add new features to existing web applications.
         </li>
-        <li className="text-base flex gap-2 text-textDark">
+        <li className="text-base flex gap-2">
             <span className="text-textGreen mt-1"></span>
             Build and maintain web applications using ReactJS, NextJS, and TailwindCSS.
         </li>
