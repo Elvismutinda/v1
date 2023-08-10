9bd48ebb101bfe058d8e2fc636ee6d795cf9cae3
Elvismutinda <elvocool@gmail.com>
2023-08-10 20:14:35 +0300

    Modified page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,18 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
-import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0 ">
-
-      {/* Header */}
-      <Header />
-
-      {/* Hero */}
-      <section id="hero" className="snap-start">
-        <Hero />
-      </section>
-
-      {/* About Me */}
-      <section id="about" className="snap-center">
-        <About />
-      </section>
-
-      {/* Experience */}
-      <section id="experience" className="snap-center">
-        <Experience />
-      </section>
-
-      {/* Skills */}
-      
-      {/* Projects */}
-      <section id="projects" className="snap-center">
-        <Projects />
-      </section>
-
-      {/* Contact Me */}
-      <section id="contactMe" className="snap-center">
-        <Contact />
-      </section>
-    </div>
+    <main className="flex flex-col items-center px-4">
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Contact />
+    </main>
   );
 };
 
