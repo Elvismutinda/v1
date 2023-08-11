a64cb6ac80ce097a0ee76358ee50a75382f44667
Elvismutinda <elvocool@gmail.com>
2023-08-11 11:01:43 +0300

    Added a new structure for the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: 'Elvis Mutinda',
-  description: 'My personal website'
-}
+export const metadata = {
+  title: "Elvis Mutinda",
+  description: "My personal website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="!scroll-smooth">
+      <body
+        className={`${inter.className} bg-gray-950 text-gray-50 text-opacity-90 relative pt-28 sm:pt-36`}
+      >
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+          <Footer />
+
+          <Toaster position="top-right" />
+        </ActiveSectionContextProvider>
+      </body>
     </html>
-  )
+  );
 }
