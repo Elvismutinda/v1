8d917f560d32174874347da4e74121c9af5b65c0
Elvismutinda <elvocool@gmail.com>
2023-08-11 13:09:28 +0300

    Added experience section

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,83 +1,17 @@
 "use client";
-import React, { useState } from "react";
-import Intellisoft from "./workExperience/Intellisoft";
-import AmazonTest from "./workExperience/AmazonTest";
-import GoogleTest from "./workExperience/GoogleTest";
-import { motion } from "framer-motion";
 
-type Props = {};
+import React from "react";
+import { experiencesData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import SectionHeading from "./SectionHeading";
 
-function ExperienceTest({}: Props) {
-  const [workIntellisoft, setWorkIntellisoft] = useState(true);
-  const [workAmazonTest, setWorkAmazonTest] = useState(false);
-  const [workGoogleTest, setWorkGoogleTest] = useState(false);
-
-  const handleIntellisoft = () => {
-    setWorkIntellisoft(true);
-    setWorkAmazonTest(false);
-    setWorkGoogleTest(false);
-  };
-
-  const handleAmazon = () => {
-    setWorkIntellisoft(false);
-    setWorkAmazonTest(true);
-    setWorkGoogleTest(false);
-  };
-
-  const handleGoogle = () => {
-    setWorkIntellisoft(false);
-    setWorkAmazonTest(false);
-    setWorkGoogleTest(true);
-  };
+export default function Experience() {
+  const { ref } = useSectionInView("Experience");
 
   return (
-    <motion.div className="w-full md:h-screen p-2 flex items-center py-16">
-      <div className="max-w-[1240px] m-auto  gap-8">
-        <h3 className="uppercase tracking-widest text-gray-500 text-4xl">
-          Work Experience
-        </h3>
-        <div className="items-center justify-center mt-10 flex flex-row overflow-hidden">
-          <div>
-            <ul className="md:w-32 flex flex-col">
-              <li
-                onClick={handleIntellisoft}
-                className={`${
-                  workIntellisoft
-                    ? "border-2 border-[#64ffda] text-[#64ffda]"
-                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
-                }  border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-              >
-                IntelliSOFT Consulting Limited
-              </li>
-              <li
-                onClick={handleAmazon}
-                className={`${
-                  workAmazonTest
-                    ? "border-2 border-[#64ffda] text-[#64ffda]"
-                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
-                } border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-              >
-                Amazon test
-              </li>
-              <li
-                onClick={handleGoogle}
-                className={`${
-                  workGoogleTest
-                    ? "border-2 border-[#64ffda] text-[#64ffda]"
-                    : "border-2 hover:border-gray-700 text-gray-700 hover:text-[#64ffda]"
-                } border-y-0 border-r-0 border-l-4 border-gray-700 bg-transparent hover:bg-gray-800 py-3 text-sm cursor-pointer duration-300 px-8 font-medium`}
-              >
-                Google test
-              </li>
-            </ul>
-          </div>
-          {workIntellisoft && <Intellisoft />}
-          {workAmazonTest && <AmazonTest />}
-          {workGoogleTest && <GoogleTest />}
-        </div>
-      </div>
-    </motion.div>
+    <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
+      <SectionHeading>Experience</SectionHeading>
+
+    </section>
   );
 }
-
-export default ExperienceTest;
