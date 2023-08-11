601bccf839de93cbae2ffe3160aac6358e5d3928
Elvismutinda <elvocool@gmail.com>
2023-08-11 11:20:57 +0300

    Added projects section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+"use client";
 
-type Props = {}
+import React from "react";
+import { projectsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import Project from "./Project";
+import SectionHeading from "./SectionHeading";
 
-const Projects = (props: Props) => {
+export default function Projects() {
+  const { ref } = useSectionInView("Projects", 0.5);
+
   return (
-    <div>Projects</div>
-  )
+    <section ref={ref} id="projects" className="scroll-mt-28 mb-28">
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
 }
-
-export default Projects;
