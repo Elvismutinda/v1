3706e8135cbcdc3b08d924e2e6430c960dde0c27
Elvismutinda <elvocool@gmail.com>
2023-08-21 10:26:55 +0300

    Added Link to download cv

--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -5,6 +5,7 @@ import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import Experience from "./Experience";
 import SectionHeading from "./SectionHeading";
+import { FiArrowRight } from "react-icons/fi";
 
 export default function Experiences() {
   const { ref } = useSectionInView("Experience", 0.5);
@@ -18,6 +19,20 @@ export default function Experiences() {
             <Experience {...experience} />
           </React.Fragment>
         ))}
+        <div className="mt-12">
+          <a
+            href="/Elvis Mutinda CV.pdf"
+            download
+            className="text-slate-200 inline-flex items-center leading-tight font-semibold group"
+          >
+            <span className="border-b border-transparent pb-px transition group-hover:border-teal-300 motion-reduce:transition-none">
+              View Full Resume
+            </span>
+            <span className="whitespace-nowrap">
+              <FiArrowRight className="ml-1 inline-block h-4 w-4 shrink-0 -translate-y-px transition-transform group-hover:translate-x-1 group-focus-visible:translate-x-1 motion-reduce:transition-none" />
+            </span>
+          </a>
+        </div>
       </div>
     </section>
   );
