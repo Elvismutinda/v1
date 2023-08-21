61ab5a3fb8ea40f4594fa813e397101e6727a226
Elvismutinda <elvocool@gmail.com>
2023-08-21 16:15:36 +0300

    Added transluscent styling on table header

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -35,7 +35,7 @@ export default function ArchivePage() {
           All Projects
         </h1>
         <table className="mt-12 w-full border-collapse text-left">
-          <thead className="sticky top-0 z-10 border-b border-slate-300/10 bg-gray-950 px-6 py-5 backdrop-blur">
+          <thead className="sticky top-0 z-10 border-b border-slate-300/10 bg-slate-90/75 px-6 py-5 backdrop-blur">
             <tr>
               <th className="py-4 pr-8 text-sm font-semibold text-slate-200">
                 Year
